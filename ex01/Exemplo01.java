e8d22e84af2ae151a56de1057a81926ae4c95baa
Pauloph <139489403+Pauloph89@users.noreply.github.com>
2025-08-16 00:09:06 +0000

    REVISÃO FOR

---
+++ ex01/Exemplo01.java
@@ -0,0 +1,12 @@
+public class Exemplo01 {
+    public static final int QTD_REPETICOES = 5;
+
+    public static void main(String[] args) {
+        System.out.println("Olá, Turma!");
+        int numero;
+
+        for(int i=1; i>= QTD_REPETICOES; i+=1){
+            System.out.println(i + ". Olá, turma!");
+        }
+    }
+}
